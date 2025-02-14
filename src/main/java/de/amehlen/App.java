15fd34c8ea049158e0f1cb15f5fe192bc37a35ab
Allan Mehlen <mehlen.allan@gmail.com>
2025-02-14 08:44:39 +0100

    chore: setup new maven project

---
+++ src/main/java/de/amehlen/App.java
@@ -0,0 +1,13 @@
+package de.amehlen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
