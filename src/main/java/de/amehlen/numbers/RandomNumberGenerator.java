a3766757b8fc63ebf07c03535cf34055981b3dd1
Allan Mehlen <mehlen.allan@gmail.com>
2025-02-18 12:19:42 +0100

    refactor(number generator): use functional programming to generate a range of random numbers

--- src/main/java/de/amehlen/numbers/RandomNumberGenerator.java
+++ src/main/java/de/amehlen/numbers/RandomNumberGenerator.java
@@ -1,6 +1,7 @@
 package de.amehlen.numbers;
 
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class RandomNumberGenerator implements NumberGenerator {
 
@@ -8,12 +9,8 @@ public class RandomNumberGenerator implements NumberGenerator {
 
   @Override
   public int[] generate(int count, int min, int max) {
-    int[] numbers = new int[count];
-
-    for (int i = 0; i < numbers.length; i++) {
-      numbers[i] = random.nextInt(max - min + 1) + min;
-    }
-
-    return numbers;
+    return IntStream.generate(() -> random.nextInt((max - min + 1) + min))
+        .limit(count)
+        .toArray();
   }
 }
